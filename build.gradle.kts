114c928534f3ab9968109dd02108cabfea5bd6b4
hwangseonu <hwangseonu12@naver.com>
2020-12-23 00:07:26 +0900

    jitpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.4.21"
     id("com.github.johnrengelman.shadow") version "5.2.0"
+    `maven-publish`
 }
 
 group = "me.mocha"
@@ -8,11 +9,20 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    mavenLocal()
     maven("https://papermc.io/repo/repository/maven-public/")
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
     compileOnly("com.destroystokyo.paper", "paper-api", "1.16.4-R0.1-SNAPSHOT")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("EconomySystem") {
+            artifactId = project.name
+            from(components["java"])
+            artifact(tasks.shadowJar)
+        }
+    }
 }
